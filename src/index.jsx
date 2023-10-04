2c11e2d83e11f6d819dd736751618ae91d8a4fb1
Mariusz Łotocki <mk.lotocki@gmail.com>
2023-10-04 16:37:03 +0200

    Initialize files on branch crud_list_rebuild

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import Provider from "./Store/TaskContext.jsx";
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
     <React.StrictMode>
       <Provider>
         <App />
